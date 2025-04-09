b4e0ee47df72f0a3d2b344829846540080556ac9
toth-marcell <toth.marcell.21i@egressy.info>
2025-04-09 14:12:01 +0200

    Add project files.

---
+++ Pancakes/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pancakes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Pancake> pancakes = new List<Pancake>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (KeyValuePair<string, double> dough in Pancake.DoughTypes) doughField.Items.Add(new ComboBoxItem { Content = dough.Key });
+            foreach (KeyValuePair<string, double> filling in Pancake.FillingTypes) fillingField.Items.Add(new ComboBoxItem { Content = filling.Key });
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            Pancake pancake = new Pancake(nField.Number, doughField.Text, fillingField.Text);
+            pancakes.Add(pancake);
+            outputPanel.Children.RemoveAt(outputPanel.Children.Count - 1);
+            outputPanel.Children.Add(new Label { Content = pancake.ToString() });
+            outputPanel.Children.Add(new Label { Content = "Végösszeg: " + pancakes.Select(p => p.Price).Sum() + " Ft" });
+        }
+    }
+    struct Pancake
+    {
+        public static int BasePrice = 100;
+        public static Dictionary<string, double> DoughTypes = new Dictionary<string, double>
+        {
+            ["normál"] = 1,
+            ["nem normál"] = 1.2
+        };
+        public static Dictionary<string, double> FillingTypes = new Dictionary<string, double>
+        {
+            ["kakaós"] = 1,
+            ["túrós"] = 1,
+            ["lekváros"] = 1
+        };
+
+        public int N { get; set; }
+        public string Dough { get; set; }
+        public string Filling { get; set; }
+        public int Price => (int)(BasePrice * N * FillingTypes[Filling] * DoughTypes[Dough]);
+
+        public Pancake(int n, string dough, string filling)
+        {
+            N = n;
+            Dough = dough;
+            Filling = filling;
+        }
+        public override string ToString() => $"{N} db {Dough} tésztás {Filling} palacsinta\t{Price} Ft";
+    }
+}
---
+++ Pancakes/NumberBox.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pancakes
+{
+    /// <summary>
+    /// Interaction logic for NumberBox.xaml
+    /// </summary>
+    public partial class NumberBox : UserControl
+    {
+        public int Number
+        {
+            get => int.Parse(textbox.Text);
+            set
+            {
+                if (value > 0) textbox.Text = value.ToString();
+                else textbox.Text = "1";
+            }
+        }
+        public NumberBox()
+        {
+            InitializeComponent();
+        }
+        private void plusButton_Click(object sender, RoutedEventArgs e) => Number++;
+        private void minusButton_Click(object sender, RoutedEventArgs e) => Number--;
+        string PrevText = "";
+        private void textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int output;
+            if (int.TryParse(textbox.Text, out output) && output > 0)
+            {
+                PrevText = textbox.Text;
+            }
+            else
+            {
+                int PrevSelectStart = textbox.SelectionStart;
+                int PrevSelectLength = textbox.SelectionLength;
+                textbox.Text = PrevText;
+                textbox.SelectionStart = PrevSelectStart;
+                textbox.SelectionLength = PrevSelectLength;
+            };
+
+        }
+    }
+}
